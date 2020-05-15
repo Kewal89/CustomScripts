0407f97e6984e1ea37923ce7befc76c8114104f5
Kewal Rathod <kewalrathod8@gmail.com>
2020-05-15 22:01:45 +0530

    * Improved rn-generate-apk *
    * Added Menu For Ez Pz Trix. Ty

--- react-native/windows/rn-generate-apk.bat
+++ react-native/windows/rn-generate-apk.bat
@@ -1,16 +1,36 @@
 @ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:MENU
+ECHO.
+ECHO ============================================================
+ECHO Run This Only In Your RN Project Root Directory
+ECHO ============================================================
+ECHO.
 ECHO ========== GENERATE APK ==========
-set /p a="Debug (D) Or Release (R) APK ? : "
-IF %a%==D (
-    echo Generating Debug APK... 
-    cmd /k "react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res && cd android && gradlew assembleDebug"
-)  ELSE (
-    IF %a%==R (
-        echo Generating Release APK...
-        cmd /k "react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res && cd android && gradlew assembleRelease"
-    )   ELSE (
-        echo Invalid Input... Exiting...
-    )
-)
-ECHO ========== GENERATED APK ==========
+ECHO 1 - GENERATE DEBUG APK
+ECHO 2 - GENERATE RELEASE APK
+ECHO 3 - Exit
+ECHO.
+SET /P "Menu=Enter Your Choice : "
+IF %Menu%==1 GOTO DEBUG
+IF %Menu%==2 GOTO RELEASE
+IF %Menu%==3 GOTO EXIT_FFS
+
+
+:DEBUG
+ECHO Generating Debug APK... 
+CMD /k "react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res && cd android && gradlew assembleDebug"
+ECHO ========== GENERATED DEBUG APK ==========
+GOTO MENU
+
+:RELEASE
+ECHO Generating Release APK...
+CMD /k "react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res && cd android && gradlew assembleRelease"
+ECHO ========== GENERATED RELEASE APK ==========
+GOTO MENU
+
+:EXIT_FFS
+ECHO Exiting
 PAUSE
+
