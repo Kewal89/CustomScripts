932f6776c85b07a85196744a93a84663a3a7e0a6
Kewal69 <43823249+Kewal89@users.noreply.github.com>
2020-05-16 06:04:13 +0530

    * Improved rn-generate-apk.bat *
    * Now Open Folder Once APK Generated.

--- react-native/windows/rn-generate-apk.bat
+++ react-native/windows/rn-generate-apk.bat
@@ -22,15 +22,16 @@ IF %Menu%==3 GOTO EXIT_FFS
 ECHO Generating Debug APK... 
 CMD /k "react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res && cd android && gradlew assembleDebug"
 ECHO ========== GENERATED DEBUG APK ==========
+START CMD /k "cd android\app\build\outputs\apk\debug && start . && EXIT"
 GOTO MENU
 
 :RELEASE
 ECHO Generating Release APK...
 CMD /k "react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res && cd android && gradlew assembleRelease"
 ECHO ========== GENERATED RELEASE APK ==========
+START CMD /k "cd android\app\release && start . && EXIT"
 GOTO MENU
 
 :EXIT_FFS
 ECHO Exiting
 PAUSE
-
